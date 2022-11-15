bef1ad434d16c1ee5b92fcc6cd78bd9fd960e7aa
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2022-11-16 02:10:53 +0530

    Model works

---
+++ models/cifar/__init__.py
@@ -0,0 +1,12 @@
+import os
+import torch
+
+
+def get_model():
+    curr_path = os.path.realpath(os.path.dirname(__file__))
+    model_path = os.path.join(curr_path, "vgg_chkpnt.pth")
+    model = torch.load(model_path)
+    return model
+
+
+print("Yoyo")

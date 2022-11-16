ff3e5099e45e9e02dc473df4ec855fddd41dead7
Tzu-Ching Yen <thomson.yen@mail.utoronto.ca>
2022-11-16 09:13:25 -0500

    Allow executation of test files as scripts from parent folder

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+parent_dir = os.getcwd()
+sys.path.append(parent_dir)
--- tests/augmentation_tests.py
+++ tests/augmentation_tests.py
@@ -1,10 +1,15 @@
-from torchvision.datasets import CIFAR10
-import torchvision.transforms as transforms
-
-from datasets import get_torchvision_dataset, get_torchvision_dataset_sample, InverseNormalize, BatchNormalize
-import utils
-from defence import DefendedNetwork
+import __init__  # Allow executation of this file as script from parent folder
 import matplotlib.pyplot as plt
+from torchvision.datasets import CIFAR10
+
+import utils
+from datasets import (
+    BatchNormalize,
+    InverseNormalize,
+    get_torchvision_dataset,
+    get_torchvision_dataset_sample,
+)
+from defence import DefendedNetwork
 
 
 def test_augmentation_visual(Aug_class, dataset=CIFAR10, param_sets=[], batch_size=5):
@@ -21,8 +26,11 @@ def test_augmentation_visual(Aug_class, dataset=CIFAR10, param_sets=[], batch_si
         imgs = []
         for i in range(batch_size):
             imgs.append([image_X[i], auged_X[i]])
-        utils.show_grid(imgs, title=f"Noise on {dataset.__name__}|({param_set})",
-                        captions=[["Image", "Augmented Image"] for i in range(batch_size)])
+        utils.show_grid(
+            imgs,
+            title=f"Noise on {dataset.__name__}|({param_set})",
+            captions=[["Image", "Augmented Image"] for i in range(batch_size)],
+        )
 
 
 def dict_str(d):
@@ -31,7 +39,15 @@ def dict_str(d):
         s += f"|{k}:{d[k]}|"
 
 
-def test_augmentation_defence(model, Aug_class, dataset=CIFAR10, param_sets=[], output_shape=(10, ), sample_rate=10, batch_size=50):
+def test_augmentation_defence(
+    model,
+    Aug_class,
+    dataset=CIFAR10,
+    param_sets=[],
+    output_shape=(10,),
+    sample_rate=10,
+    batch_size=50,
+):
     device = utils.Parameters.device
     X, y = get_torchvision_dataset_sample(dataset, batch_size=batch_size)
     X, y = X.to(device), y.to(device)
@@ -43,9 +59,19 @@ def test_augmentation_defence(model, Aug_class, dataset=CIFAR10, param_sets=[], 
     for params in param_sets:
         aug = Aug_class(**params)
         param_list.append(str(params))
-        defended = DefendedNetwork(network=model, data_augmentation=aug, data_n_dims=3, transform=batch_transform,
-                                   inverse_transform=inverse_transform, output_shape=output_shape,
-                                   sample_rate=sample_rate)
+        defended = DefendedNetwork(
+            network=model,
+            data_augmentation=aug,
+            data_n_dims=3,
+            transform=batch_transform,
+            inverse_transform=inverse_transform,
+            output_shape=output_shape,
+            sample_rate=sample_rate,
+        )
         accuracies.append(utils.get_accuracy_logits(y, defended(X)))
     plt.plot(param_list, accuracies)
     plt.show()
+
+
+if __name__ == "__main__":
+    print("yoyo")

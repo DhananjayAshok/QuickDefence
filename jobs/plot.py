08eb0c609ec5cfdaa631da32d4c57c4db8c1847b
Tzu-Ching Yen <thomson.yen@mail.utoronto.ca>
2022-12-04 20:37:03 -0500

    Add results in plots

--- jobs/plot.py
+++ jobs/plot.py
@@ -7,6 +7,23 @@ from pathlib import Path
 
 FOLDER = Path(parent_path) / "results" / "slide"
 FOLDER.mkdir(parents=True, exist_ok=True)
+E1_DF = pd.read_csv(parent_path + "/NewExperiment1.csv")
+E2_DF = pd.read_csv(parent_path + "/NewExperiment2.csv")
+
+TRAINING_CODE = {
+    "U": "Regular Training",
+    "A": "Augmented Training"
+}
+
+TTA_CODE =  {
+    "U": "No Test Time Augmentations",
+    "N": "Noise Time Augmentations",
+    "A": "All Time Augmentations",
+}
+
+NOISE = "Noise"
+TRANS = "Translation"
+ROTAT = "Rotation"
 
 def get_numpy_from_string(tstr: str):
     tstr = tstr[tstr.find("(")+1:]
@@ -63,7 +80,7 @@ def plot_slide2(df):
     print(f"{def_acc=}")
     print(f"{ndef_acc=}")
 
-    plt.xlabel("Attack l2 norm")
+    plt.xlabel("PGD Attack l2 norm")
     plt.ylabel("Accuracy")
     plt.plot(attack_int, def_acc, label="w/ Test Time Augmentations")
     plt.plot(attack_int, ndef_acc, label="w/o Test Time Augmentations")
@@ -71,22 +88,93 @@ def plot_slide2(df):
     plt.savefig(FOLDER / f"s2_model={model}.png")
 
 def plot_slide3(df):
-    aug = "Noise"
+    aug = "Translation"
     model = "U" # U for non-robust. A for robust
     strengths = get_e2_augmentation_strength(df, augmentation=aug)
     clean_acc = get_e2_acc(df, model=model, is_adv=False, augmentation=aug)
     adv_acc = get_e2_acc(df, model=model, is_adv=True, augmentation=aug)
-    plt.xlabel("Test-time noise augmentation strengths")
+    plt.xlabel(f"Test-time {aug.lower()} augmentation strengths")
     plt.ylabel("Accuracy")
     plt.plot(strengths, clean_acc, label="Clean Accuracy")
     plt.plot(strengths, adv_acc, label="Advesarial Accuracy")
     plt.legend()
-    plt.savefig(FOLDER / f"s3_model={model}.png")
+    plt.savefig(FOLDER / f"s3_model={model}-aug={aug.lower()}.png")
+
+def plot_a1():
+    """Do additional augmentations help?
+    Fix model training, try different test time augmentations
+    """
+    model = "U" # U for non-robust. A for robust
+
+    attack_int = get_e1_attack_intensity(E1_DF)
+    defall_acc = get_e1_accuracies(E1_DF, model + "A")
+    defnoise_acc = get_e1_accuracies(E1_DF, model + "N")
+    ndef_acc = get_e1_accuracies(E1_DF, model + "U")
+
+    plt.xlabel("PGD Attack l2 norm")
+    plt.ylabel("Accuracy")
+    plt.plot(attack_int, defall_acc, label="w/ All Test Time Augmentations")
+    plt.plot(attack_int, defnoise_acc, label="w/ Noise Test Time Augmentations")
+    plt.plot(attack_int, ndef_acc, label="w/ No Test Time Augmentations")
+    plt.legend()
+
+    folder = FOLDER / "a1"
+    folder.mkdir(parents=True, exist_ok=True)
+    plt.savefig(folder / f"model={model}.png")
+
+def plot_a2():
+    """Do training help?
+    Fixed test time augmentations, try different training processes
+    """
+    tta = "N" # U for no test time augmentations, A for all tta, N for noise tta
+
+    attack_int = get_e1_attack_intensity(E1_DF)
+    rob_acc = get_e1_accuracies(E1_DF, "A" + tta)
+    nrob_acc = get_e1_accuracies(E1_DF, "U" + tta)
+
+    plt.title(f"w/ {TTA_CODE[tta]}")
+    plt.xlabel("PGD Attack l2 norm")
+    plt.ylabel("Accuracy")
+    plt.plot(attack_int, rob_acc, label="w/ Augmented Training")
+    plt.plot(attack_int, nrob_acc, label="w/ Regular Training")
+    plt.legend()
+
+    folder = FOLDER / "a2"
+    folder.mkdir(parents=True, exist_ok=True)
+    plt.savefig(folder / f"tta={tta}.png")
+
+def plot_a3():
+    """How robust to TTA are clean and advsarial imagse?
+    Fixed type of augmentations and training processes
+    try clean and advsarial accuracies
+    """
+    aug_type = TRANS
+    model = "A" # U for non-robust. A for robust
+
+    for aug_type in [NOISE, TRANS, ROTAT]:
+        for model in ["U", "A"]:
+            clean_acc = get_e2_acc(E2_DF, model=model, is_adv=False, augmentation=aug_type)
+            adv_acc = get_e2_acc(E2_DF, model=model, is_adv=True, augmentation=aug_type)
+            strengths = get_e2_augmentation_strength(E2_DF, augmentation=aug_type)
+
+            plt.title(f"Type of Augmentation: {aug_type}")
+            plt.xlabel(f"Test-time augmentation strengths")
+            plt.ylabel("Accuracy")
+
+            plt.plot(strengths, clean_acc, label="Clean Accuracy")
+            plt.plot(strengths, adv_acc, label="Advesarial Accuracy")
+            plt.legend()
+
+            folder = FOLDER / "a3"
+            folder.mkdir(parents=True, exist_ok=True)
+            plt.savefig(folder / f"aug={aug_type}_model={model}.png")
+            plt.clf()
+
 
 if __name__ == "__main__":
-    csv1 = pd.read_csv(parent_path + "/NewExperiment1.csv")
-    csv2 = pd.read_csv(parent_path + "/NewExperiment2.csv")
 
-    # plot_slide1(csv1)
-    plot_slide2(csv1)
-    #plot_slide3(csv2)
+    # plot_slide1(E1_DF)
+    # plot_slide2(E1_DF)
+    # plot_slide3(E2_DF)
+    # plot_a1()
+    plot_a2()
